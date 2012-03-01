383c46a0529aeadc52fe35db3f59969428df78da
gdacarv <gdacarv@gmail.com>
2012-03-01 20:03:02 +0000

    Friction on/off;

---  gdacarv-android-game-engine/src/com/gdacarv/engine/androidgame/PhysicalSprite.java
+++  gdacarv-android-game-engine/src/com/gdacarv/engine/androidgame/PhysicalSprite.java
@@ -5,7 +5,8 @@ import android.graphics.Bitmap;
 public class PhysicalSprite extends Sprite {
 
 	public Vector Movement, Acceleration;
-	public float Mass = 1, FrictionX = 0.7f, FrictionY = 1f;
+	public float Mass = 1, FrictionX = 0.7f;
+	public boolean FrictionXOn;
 	
 	public PhysicalSprite(Bitmap bmp) {
 		this(bmp, 1, 1);
@@ -15,6 +16,7 @@ public class PhysicalSprite extends Sprite {
 		super(bmp, bmp_rows, bmp_columns);
 		Movement = new Vector();
 		Acceleration = new Vector();
+		FrictionXOn = true;
 	}
 	
 	@Override
@@ -27,9 +29,11 @@ public class PhysicalSprite extends Sprite {
 	}
 
 	private void applyFriction() {
-		Movement.multX(FrictionX);
-		if(Math.abs(Movement.getX()) < 1f)
-			Movement.setX(0f);
+		if(FrictionXOn){
+			Movement.multX(FrictionX);
+			if(Math.abs(Movement.getX()) < 1f)
+				Movement.setX(0f);
+		}
 	}
 
 	public void applyForce(Vector force){
